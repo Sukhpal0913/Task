5449a5523f1481557e0ddfdfd747023bf12fbfef
Sukhpal0913 <47098255+Sukhpal0913@users.noreply.github.com>
2019-02-06 19:00:22 +0530

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,51 @@
+
+var answer = [
+    {
+        obj1:"Client side scripting language",
+    },
+    {
+        obj2: "Yes",
+
+    },
+    {
+        obj3: "No", 
+    },
+    {
+        obj4: "console.log();",
+    },
+    {
+        obj5: "var arr1: [a,e,i,o,u]",
+    },
+    {
+        obj6: "Function without a name",  
+    },
+    {
+        obj7: "sort()",
+    },
+
+    {
+        obj8:"object",
+    },
+    {
+        obj9:"function myFunction",
+    },
+    {
+        obj10:"if(i == 3)"
+    }
+
+
+];
+
+function quiz(ans) {
+    if(ans == 1) {
+        console.log("Correct ans");
+    } else {
+        console.log('wrong ans');
+    }
+}
+
+
+
+
+
+
