c815a67021bbe2ab767e5191adeaeef9725298b5
Sukhpal0913 <47098255+Sukhpal0913@users.noreply.github.com>
2019-02-07 18:24:14 +0530

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,44 +1,177 @@
 
-var answer = [
-    {
-        obj1:"Client side scripting language",
-    },
-    {
-        obj2: "Yes",
+let data = {
+    count:0,
+    check: false,
+    s: '2'
+}   
+   
 
-    },
-    {
-        obj3: "No", 
-    },
-    {
-        obj4: "console.log();",
-    },
-    {
-        obj5: "var arr1: [a,e,i,o,u]",
-    },
-    {
-        obj6: "Function without a name",  
-    },
-    {
-        obj7: "sort()",
-    },
+// window.localStorage.setItem("count", 0);
 
-    {
-        obj8:"object",
-    },
-    {
-        obj9:"function myFunction",
-    },
-    {
-        obj10:"if(i == 3)"
+// //In another page:
+// function count(){
+//     localStorage.setItem("count", parseInt(window.localStorage.getItem("count")) );
+//     var c  = parseInt(window.localStorage.getItem("count")) + 1;
+//     console.log("-----"+ c);
+//     localStorage.setItem("count", parseInt(window.localStorage.getItem("count")) + 1);
+// }
+
+
+// //localStorage.setItem("count", 0);
+// var questions = [
+//     {
+//         ques1: "What is JavaScript?",
+
+//     }
+// ]
+
+ 
+
+// var u = document.getElementById("a").href;
+// var ind = u.indexOf('.');
+// data.s = u.charAt(ind - 1);
+
+ function listener1(){
+
+     document.querySelector('.parent').addEventListener('change', event=>{
+
+         console.log(event.target);
+        data.id =event.target.getAttribute('id');
+        data.value = document.getElementById(`${data.id}`).value;
+        console.log(data.value);
+       
+
+        quiz();
+       
+        
+    });
+  }
+
+  function listener2(){
+
+    document.querySelector('.parent').addEventListener('click', event=>{
+event.preventDefault();
+        console.log(event.target.parentNode);
+       let cla = event.target.parentNode.getAttribute('id');
+       console.log(cla);
+       console.log(typeof(cla));
+       let linkclass =event.target.getAttribute('class');
+       if(linkclass === 'link'){
+        document.getElementById(cla).style.display = 'none';
+       }
+       
+        
+    //    data.id =event.target.getAttribute('id');
+    //    data.value = document.getElementById(`${data.id}`).value;
+    //    console.log(data.value);
+      
+
+      
+      
+       
+       
+   });
+
+ }
+
+
+listener1();
+listener2();
+
+
+
+function quiz() {
+
+    console.log("inside method"+ data.value);
+
+    
+    if( data.value==1) {
+        console.log("Correct ans");
+        data.count++;
+        data.count++;
+        
+        //answer1.push(1);
+        //count++;
+        console.log(data.count);
+    } else {
+       // data.count++;
+        //console.log(data.count);data.count++;
+        
+        //answer1.push(1);
+        //count++; 
+        data.count++;
+        console.log(data.count);
+
+        console.log('wrong ans');
+      
     }
+}
 
+function quiz2() {
+    
+    if(data.value == 1) {
+        console.log(parseInt(localStorage.getItem("count")));
+        console.log("Correct ans");
+        count();
+        //answer1.push(1);
+     //   count++;
+       // console.log(count);
+    } else {
+        console.log('wrong ans');
+    }
+}
 
-];
+function quiz3(ans) {
+    if(ans == 2) {
+        console.log("Correct ans");
+        //answer1.push(1);
+        data.count++;
+        console.log(data.count);
+    } else {
+        console.log('wrong ans');
+    }
+}
 
-function quiz(ans) {
+function quiz4(ans) {
+    if(ans == 2) {
+        console.log("Correct ans");
+        //answer1.push(1);
+        data.count++;
+        console.log(data.count);
+    } else {
+        console.log('wrong ans');
+    }
+}
+
+function quiz5(ans) {
     if(ans == 1) {
         console.log("Correct ans");
+        //answer1.push(1);
+        data.count++;
+        console.log(data.count);
+    } else {
+        console.log('wrong ans');
+    }
+}
+
+function quiz6(ans) {
+    if(ans == 2) {
+        console.log("Correct ans");
+       //answer1.push(1);
+       data.count++;
+       console.log(data.count);
+    } else {
+        console.log('wrong ans');
+    }
+}
+
+function quiz7(ans) {
+    if(ans == 3) {
+        console.log("Correct ans");
+      //answer1.push(1);
+      
+      data.count++;
+      console.log(data.count);
     } else {
         console.log('wrong ans');
     }
@@ -46,6 +179,89 @@ function quiz(ans) {
 
 
 
+function quiz8(ans) {
+    if(ans == 3) {
+        
+        console.log("Correct ans");
+        //answer1.push(1);
+        data.count++;
+        console.log(data.count);
+    } else {
+        console.log('wrong ans');
+    }
+}
+
+function quiz9(ans) {
+    if(ans == 2) {
+        console.log("Correct ans");
+        //answer1.push(1);
+        data.count++;
+        console.log(data.count);
+    } else {
+        console.log('wrong ans');
+    }
+}
+
+function quiz10(ans) {
+    if(ans == 3) {
+        console.log("Correct ans");
+      //answer1.push(1);
+      data.count++;
+      console.log(data.count);
+    } else {
+        console.log('wrong ans');
+    }
+}
+// list();
+/*
+function result() {
+    console.log("this is it");
+    console.log("Total correct answers are: " + data.count);
+}result();
+
+*/
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*var answer = ['c','y','n','co','v','s','o','f','i'];
+// var count=0;
+// function quiz(id){
+//     var correct=false;
+//     answer.forEach(el=>{ 
+//         if(el==id){
+//         correct=true
+//         }
+//     });
+//     console.log(id);
+// if(correct)
+// {
+//     count++;
+//     alert(count);
+// }else{
+//     alert("wrong answer");
+// }
